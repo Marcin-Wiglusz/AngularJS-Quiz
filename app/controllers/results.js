d87ad5e52b6513fd827e5cf31045435ddb435d19
Marcin-Wiglusz <marcin.m.wiglusz@gmail.com>
2017-12-19 02:18:31 +0100

    adding end score

--- app/controllers/results.js
+++ app/controllers/results.js
@@ -12,6 +12,25 @@
       this.quizFactory = quizFactoryData;
       this.questionsService = questionsServiceData;
       this.activeQuestion = 0
+
+      this.setActiveQuestion = function setActiveQuestion(index) {
+        this.activeQuestion = index;
+      }
+
+      this.getAnswerClass = function getAnswerClass(index) {
+
+        if (index === quizFactoryData.correctAnswers[this.activeQuestion]) {
+          return 'bg-success';
+        }
+        else if (index === questionsServiceData.quizQuestions[this.activeQuestion].selected) {
+          return 'bg-danger';
+        }
+      }
+
+      this.endScorePerc = function endScorePerc() {
+
+        return quizFactoryData.sumOfCorrAnswers / questionsServiceData.quizQuestions.length * 100;
+      }
     }
 
 })();
